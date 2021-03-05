2043d44fb2091783268a70ffba94d706cd6963a3
kerwin kow <e0177044@u.nus.edu>
2021-03-05 16:31:28 +0800

    Final COmmit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
 
-function Profile() {
-  const loginStyle = {
-    color: "black",
-    textDecoration: "none",
-  };
+import ApiService from "../services/apiServices";
 
-  return (
-    <div>
-      <h1>Profile Page</h1>
-    </div>
-  );
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accountKey: " ",
+      address: " ",
+      email: " ",
+      firstName: " ",
+      lastName: " ",
+      nric: " ",
+      phoneNumber: " ",
+      username: " ",
+    };
+
+    ApiService.login(
+      props.location.state.username,
+      props.location.state.password
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        // console.log("*********", response);
+        this.setState({
+          firstName: response.firstName,
+          lastName: response.lastName,
+          address: response.address,
+          email: response.email,
+          phoneNumber: response.phoneNumber,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="col-md-12">
+        <div className="welcomemessage">
+          <h1>
+            Welcome {this.state.firstName} {this.state.lastName}
+          </h1>
+        </div>
+        <div className="profile_page">
+          <h2>Personal Information</h2>
+          <h4>Phone Number Registered: {this.state.phoneNumber}</h4>
+          <h4>Address: {this.state.address}</h4>
+          <h4>Email: {this.state.email}</h4>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default Profile;
--- src/components/transactions.jsx
+++ src/components/transactions.jsx
@@ -2,42 +2,96 @@ import React, { Component } from 'react';
 import ApiService from '../services/apiServices';
 
 class Transactions extends Component {
-    state = {
-        transactionHistory: 0
-    }
 
-    constructor() {
-        super();
+
+    constructor(props) {
+        super(props);
+
         this.viewTransactions = this.viewTransactions.bind(this);
-        this.viewTransactions();
-        // console.log(this.state.transactionHistory);
-    }
 
-    render() { 
-        return ( 
-            <div>
-                <button >View Transactions</button>
-            </div>
-         );
-    }
+        this.state = {
+            transactionHistory: [],
+        };
 
-    viewTransactions() {
-        console.log("Retrieving Transactions......");
-        ApiService.pastTransactions("f4481c86-1a64-49a7-b85c-3d4449eabf69")
+        // ApiService.pastTransactions("f4481c86-1a64-49a7-b85c-3d4449eabf69")
+        ApiService.pastTransactions(JSON.parse(localStorage.getItem("login_JSON")).accountKey)
             .then(
                 (response) => response.json()
             )
-            // .then(
-            //     (response) => console.log(response)
-            // )
             .then(
                 (response) => {
                     this.setState({
                         transactionHistory: response,
-                    })
+                    });
                 }
             )
+        
     }
+    render() { 
+        return ( 
+            <div>
+                {this.viewTransactions()}
+            </div>
+         );
+    }
+
+    
+
+    viewTransactions() {
+        console.log(this.state.transactionHistory);
+        return (
+            <table style={{backgroundColor: "white"}}>
+                <tr>
+                    <th>Order Type</th>
+                    <th>TimeStamp</th>
+                    <th>Asset Amount</th>
+                    <th>Asset Symbol</th>
+                    <th>Asset Price</th>
+                    <th>Cash Amount</th>
+                    <th>Transaction Id</th>
+                </tr>
+                {
+                    this.state.transactionHistory.map(
+                        (transaction) => (
+                            <tr>
+                                {this.OrderTypeFormatter(transaction.orderType)}
+                                <td>{this.timeConverter(transaction.timestamp)}</td>
+                                <td>{transaction.assetAmount}</td>
+                                <td>{transaction.assetSymbol}</td>
+                                <td>{transaction.assetPrice}</td>
+                                <td>{transaction.cashAmount}</td>
+                                <td>{transaction.transactionId}</td>
+                            </tr>
+                        )
+                    )
+                }
+            </table>
+        )
+
+    }
+
+    timeConverter(UNIX_timestamp){
+        var a = new Date(UNIX_timestamp * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
+      }
+
+    OrderTypeFormatter(BuySell) {
+        if (BuySell === "BUY") {
+            return (<td style={{color: 'blue'}}>{BuySell}</td>)
+        } else {
+            return (<td style={{color: 'red'}}>{BuySell}</td>)
+        }
+    }
+      
+
 }
  
 export default Transactions;
